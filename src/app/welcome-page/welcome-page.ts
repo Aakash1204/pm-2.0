fb0169e9c043a4c6869143596277fdf971d6cf78
Akash Mukherjee <mukherjee.akash95@gmail.com>
2025-11-20 21:49:57 +0530

    feat: implement welcome page with visual layout, image, and navigation to signup.

--- src/app/welcome-page/welcome-page.ts
+++ src/app/welcome-page/welcome-page.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-welcome-page',
-  imports: [MatButtonModule, RouterLink],
+  imports: [
+    MatButtonModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './welcome-page.html',
   styleUrls: ['./welcome-page.scss']
 })
 export class WelcomePage {
 
+  private _formBuilder = inject(FormBuilder);
+
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  isLinear = false;
 }
